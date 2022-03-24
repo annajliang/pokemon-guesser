d36955d69350da39bd6f536cbe5e6e1d5a0c9daf
annajliang <annaliangdev@gmail.com>
2022-03-24 02:08:24 -0400

    get random pokemon and render it

---
+++ components/Pokemon/Pokemon.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { useState } from "react";
+import { allPokemonState } from "../../utils/globalState";
+import { PokemonData } from "../../utils/types/interfaces";
+
+const getRandomIndex = (arr: PokemonData[]) => {
+  return Math.floor(Math.random() * arr.length);
+};
+
+export const Pokemon = () => {
+  const [allPokemon] = useRecoilState(allPokemonState);
+  const [randomPokemon, setRandomPokemon] = useState<PokemonData>();
+
+  useEffect(() => {
+    const randomIndex = getRandomIndex(allPokemon);
+    setRandomPokemon(allPokemon[randomIndex]);
+  }, [allPokemon]);
+
+  return (
+    <div>
+      {randomPokemon && (
+        <>
+          <h1>{randomPokemon.name}</h1>
+          <Image
+            src={`/pokemon/gen1/${randomPokemon.id}.png`}
+            alt={`${randomPokemon.name}`}
+            width={500}
+            height={500}
+            priority
+          />
+        </>
+      )}
+    </div>
+  );
+};
