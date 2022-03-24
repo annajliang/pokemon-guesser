bd7b0e427c9294b1e855727d87787a2d096ed875
annajliang <annaliangdev@gmail.com>
2022-03-24 03:17:18 -0400

    tell next.js to use next-optimized-images via the next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const withOptimizedImages = require("next-optimized-images");
+
+module.exports = withOptimizedImages({});
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
