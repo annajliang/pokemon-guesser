abff3d8ce5255bee456136a94b7e704607c1fe6d
annajliang <annaliangdev@gmail.com>
2022-05-02 02:52:54 -0400

    remove shown pokemon from allPokemon array to ensure no pokemon appears twice

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -2,16 +2,25 @@ import { useState, FormEvent } from 'react';
 import levenshtein from 'js-levenshtein';
 import { Input } from '../Input/Input';
 import { Button } from '../Button/Button';
-import { useRecoilValue } from 'recoil';
-import { currentPokemonState } from '../../utils/globalState';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { allPokemonState, currentPokemonState } from '../../utils/globalState';
+import { PokemonData } from '../../utils/types/interfaces';
+
+const getFilteredPokemon = (
+  allPokemon: PokemonData[],
+  currentPokemon: string
+) => {
+  return allPokemon.filter((pokemon) => {
+    return pokemon.name !== currentPokemon;
+  });
+};
 
 export const Form = () => {
   const [guess, setGuess] = useState('');
   const currentPokemon = useRecoilValue(currentPokemonState);
+  const [allPokemon, setallPokemon] = useRecoilState(allPokemonState);
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
+  const evaluateGuess = () => {
     const answer = currentPokemon.toLowerCase();
     const levenshteinDistance = levenshtein(guess, answer);
 
@@ -20,6 +29,15 @@ export const Form = () => {
     }
 
     return console.log('wrong');
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    evaluateGuess();
+
+    // remove pokemon that just appeared from allPokemon array to ensure no pokemon appears twice
+    const filteredPokemon = getFilteredPokemon(allPokemon, currentPokemon);
+    setallPokemon(filteredPokemon);
   };
 
   return (
