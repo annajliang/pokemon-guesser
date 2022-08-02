ac5d781548946ddb0c4559ad792efd1a2a2e98da
annajliang <annaliangdev@gmail.com>
2022-08-01 22:17:10 -0400

    complete barebones homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
-import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>Hello World</h1>
+      <h1>Who&apos;s that Pokemon?</h1>
+      <p>
+        It’s a race against the clock to correctly guess which one of the first
+        generation 151 Pokemon is behind the mysterious dark silhouette!
+      </p>
+      <p>
+        Earn a high score to enter the leaderboard and rank with the rest of the
+        Pokemon Elites.
+      </p>
+      <p>Time starts once you click the start button!</p>
+      <Link href="/game">
+        <a>Start</a>
+      </Link>
     </div>
   );
 };
