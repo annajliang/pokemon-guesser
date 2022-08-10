6ee5794fb53d128b510b9f6ddd2c489ebf12e103
annajliang <annaliangdev@gmail.com>
2022-08-10 03:35:36 -0400

    test setting image url ahead of time

--- components/Pokemon/Pokemon.tsx
+++ components/Pokemon/Pokemon.tsx
@@ -22,6 +22,8 @@ export const Pokemon = () => {
   const [isProcessing, setIsProcessing] = useRecoilState(isProcessingState);
   const isGuessCorrect = useRecoilValue(isGuessCorrectState);
 
+  let url;
+
   useEffect(() => {
     setRandomIndex(getRandomIndex(unseenIds));
   }, [unseenIds, setRandomIndex]);
@@ -29,8 +31,10 @@ export const Pokemon = () => {
   useEffect(() => {
     if (isProcessing) {
       setShowPokemon(true);
+      url = `/pokemon/gen${allPokemon[randomIndex].gen}/${allPokemon[randomIndex].id}.png`;
     } else {
       setShowPokemon(false);
+      url = `/pokemon/gen${allPokemon[randomIndex].gen}/${allPokemon[randomIndex].id}.png`;
     }
   }, [setShowPokemon, isProcessing]);
 
@@ -45,25 +49,23 @@ export const Pokemon = () => {
               {allPokemon[randomIndex].name}
             </StyledName>
           )}
-          <picture>
-            <source
-              srcSet={require(`../../public/pokemon/gen${allPokemon[randomIndex].gen}/${allPokemon[randomIndex].id}.png?webp`)}
-              type="image/webp"
+
+          <StyledPokemonImage
+            showPokemon={showPokemon}
+            isGuessCorrect={isGuessCorrect}
+          >
+            <Image
+              src={
+                url ||
+                `/pokemon/gen${allPokemon[randomIndex].gen}/${allPokemon[randomIndex].id}.png`
+              }
+              alt={`${allPokemon[randomIndex].name}`}
+              width={400}
+              height={400}
+              priority={true}
+              draggable="false"
             />
-            <StyledPokemonImage
-              showPokemon={showPokemon}
-              isGuessCorrect={isGuessCorrect}
-            >
-              <Image
-                src={`/pokemon/gen${allPokemon[randomIndex].gen}/${allPokemon[randomIndex].id}.png`}
-                alt={`${allPokemon[randomIndex].name}`}
-                width={400}
-                height={400}
-                priority={true}
-                draggable="false"
-              />
-            </StyledPokemonImage>
-          </picture>
+          </StyledPokemonImage>
         </>
       )}
     </div>
