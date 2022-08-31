f0d0f4e4dc3103d7ea06c9a633a0f2c5c8ea44c1
annajliang <annaliangdev@gmail.com>
2022-08-31 14:00:03 -0400

    use .map() to create arr with 151 items and render 151 pokemon images

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,39 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
+const nums = Array.from(Array(151), (_, index) => index + 1);
+
+const preloadedPokemonImages = nums.map((preloadedImage) => {
+  if (preloadedImage < 10) {
+    return (
+      <link
+        rel="preload"
+        as="image"
+        href={`/pokemon/gen1/00${preloadedImage}.png`}
+        key={preloadedImage}
+      />
+    );
+  } else if (preloadedImage >= 10 && preloadedImage < 100) {
+    return (
+      <link
+        rel="preload"
+        as="image"
+        href={`/pokemon/gen1/0${preloadedImage}.png`}
+        key={preloadedImage}
+      />
+    );
+  } else {
+    return (
+      <link
+        rel="preload"
+        as="image"
+        href={`/pokemon/gen1/${preloadedImage}.png`}
+        key={preloadedImage}
+      />
+    );
+  }
+});
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
@@ -43,34 +76,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             type="text/css"
           />
-          <link rel="preload" as="image" href={`/pokemon/gen1/001.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/002.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/003.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/004.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/005.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/006.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/007.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/008.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/009.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/010.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/011.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/012.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/013.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/014.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/015.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/016.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/017.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/018.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/019.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/020.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/021.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/022.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/023.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/024.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/025.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/026.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/027.png`} />
-          <link rel="preload" as="image" href={`/pokemon/gen1/028.png`} />
+          {preloadedPokemonImages}
         </Head>
         <body>
           <script>0</script>
