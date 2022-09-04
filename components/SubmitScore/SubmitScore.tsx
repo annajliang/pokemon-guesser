9de46f2d20e46b42964057f64064d31506c2c411
annajliang <annaliangdev@gmail.com>
2022-09-03 21:29:03 -0400

    add submit score button, use score state to render dynamic value

--- components/SubmitScore/SubmitScore.tsx
+++ components/SubmitScore/SubmitScore.tsx
@@ -1,17 +1,22 @@
+import { useRecoilValue } from 'recoil';
 import {
   StyledText,
   StyledForm,
   StyledPlayerInput,
   StyledLabel,
 } from './SubmitScore.styled';
+import { scoreState } from '../../utils/globalState';
+import { Button } from '../Button/Button';
 
 export const SubmitScore = () => {
+  const score = useRecoilValue(scoreState);
+
   return (
     <>
-      <h3>High Score</h3>
+      <h2>High Score</h2>
       <StyledText>
-        Congrats! You&apos;ve made it to the leaderboard and scored 80 points!
-        Submit your score below.
+        Congrats! You&apos;ve made it to the leaderboard and scored {score}{' '}
+        points! Submit your score below.
       </StyledText>
       <StyledForm onSubmit={(e) => e.preventDefault()}>
         <StyledLabel htmlFor="playerName">Your Name</StyledLabel>
@@ -23,6 +28,7 @@ export const SubmitScore = () => {
           maxLength={12}
           required
         />
+        <Button label="Submit" onClick={() => alert('submitted')} />
       </StyledForm>
     </>
   );
