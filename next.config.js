fe2d7cff4d1f8c942ce58e9b37522c987255cd75
annajliang <58713256+annajliang@users.noreply.github.com>
2022-09-05 15:34:04 -0400

    fix duplicate recoil key warning (#7)

--- next.config.js
+++ next.config.js
@@ -23,3 +23,17 @@ module.exports = {
     return config;
   },
 };
+
+const intercept = require('intercept-stdout');
+
+// safely ignore recoil warning messages in dev (triggered by HMR)
+function interceptStdout(text) {
+  if (text.includes('Duplicate atom key')) {
+    return '';
+  }
+  return text;
+}
+
+if (process.env.NODE_ENV === 'development') {
+  intercept(interceptStdout);
+}
