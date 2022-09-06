284e08376c00ef572f853d0ee4222db758ea7aad
annajliang <58713256+annajliang@users.noreply.github.com>
2022-09-05 23:04:29 -0400

    F/db connect (#8)
    * install mongodb
    
    * connect to mongodb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Image from 'next/image';
+import { connectToDatabase } from '../lib/mongodb';
 import { useRouter } from 'next/router';
 import { useEffect, ChangeEvent } from 'react';
 import { useApi } from '../hooks/useApi';
@@ -134,3 +135,26 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export async function getServerSideProps() {
+  try {
+    await connectToDatabase;
+    // `await clientPromise` will use the default database passed in the MONGODB_URI
+    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+    //
+    // `const client = await clientPromise`
+    // `const db = client.db("myDatabase")`
+    //
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+    console.log('You are connected to MongoDB');
+    return {
+      props: { isConnected: true },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: { isConnected: false },
+    };
+  }
+}
