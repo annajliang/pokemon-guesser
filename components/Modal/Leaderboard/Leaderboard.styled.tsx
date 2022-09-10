f55d31cae95a3a1477d0fb63b279df410197ff4a
annajliang <annaliangdev@gmail.com>
2022-09-10 16:42:12 -0400

    remove Player and Medal styled components from Leaderboard.styled.tsx

--- components/Modal/Leaderboard/Leaderboard.styled.tsx
+++ components/Modal/Leaderboard/Leaderboard.styled.tsx
@@ -9,49 +9,6 @@ export const StyledRanking = styled.ol`
   }
 `;
 
-export const StyledPlayer = styled.li`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 10px 0;
-  font-weight: 700;
-  font-size: 1.8rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid #ddd;
-  }
-
-  p {
-    padding-bottom: 0;
-    font-family: ${theme.fonts.sen};
-    font-size: 2rem;
-  }
-
-  div {
-    display: flex;
-
-    p:first-child {
-      margin-right: 40px;
-      color: ${theme.colors.heavyMetal};
-      width: 20px;
-    }
-
-    p:last-child {
-      color: ${theme.colors.heavyMetal};
-    }
-  }
-`;
-
-export const StyledMedal = styled.span`
-  position: absolute;
-  left: 26px;
-`;
-
-export const StyledScore = styled.p`
-  color: ${theme.colors.rubyRed};
-`;
-
 export const StyledText = styled.p`
   padding: 1rem 9rem 2rem;
   font-family: ${theme.fonts.sen};
