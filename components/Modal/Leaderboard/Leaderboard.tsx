816de4c2c43e35b9d3455e781914e0c57f190220
annajliang <annaliangdev@gmail.com>
2022-09-10 16:41:16 -0400

    remove Player JSX from Leaderboard

--- components/Modal/Leaderboard/Leaderboard.tsx
+++ components/Modal/Leaderboard/Leaderboard.tsx
@@ -1,13 +1,6 @@
-import Image from 'next/image';
 import { leaderboardState } from '../../../recoil';
 import { useRecoilValue } from 'recoil';
-import {
-  StyledRanking,
-  StyledPlayer,
-  StyledMedal,
-  StyledScore,
-  StyledText,
-} from './Leaderboard.styled';
+import { StyledRanking, StyledText } from './Leaderboard.styled';
 
 export const Leaderboard = () => {
   const leaderboard = useRecoilValue(leaderboardState);
@@ -17,55 +10,7 @@ export const Leaderboard = () => {
       <h2 id="modalHeading">Leaderboard</h2>
       {leaderboard.length ? (
         <StyledRanking>
-          <StyledPlayer>
-            {/* TODO: Use hardcoded data for now, remove later and use data from DB */}
-            <div>
-              <p>1</p>
-              <StyledMedal>
-                <Image
-                  src="/assets/goldMedal.svg"
-                  alt=""
-                  width={25}
-                  height={25}
-                  priority
-                />
-              </StyledMedal>
-              <p>iamwinner</p>
-            </div>
-            <StyledScore>100</StyledScore>
-          </StyledPlayer>
-          <StyledPlayer>
-            <div>
-              <p>2</p>
-              <StyledMedal>
-                <Image
-                  src="/assets/silverMedal.svg"
-                  alt=""
-                  width={25}
-                  height={25}
-                  priority
-                />
-              </StyledMedal>
-              <p>billy bob</p>
-            </div>
-            <StyledScore>90</StyledScore>
-          </StyledPlayer>
-          <StyledPlayer>
-            <div>
-              <p>3</p>
-              <StyledMedal>
-                <Image
-                  src="/assets/bronzeMedal.svg"
-                  alt=""
-                  width={25}
-                  height={25}
-                  priority
-                />
-              </StyledMedal>
-              <p>soreloser</p>
-            </div>
-            <StyledScore>50</StyledScore>
-          </StyledPlayer>
+          <></>
         </StyledRanking>
       ) : (
         <StyledText>
