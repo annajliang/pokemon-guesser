f4283dca71aa666cad253e15e3908cc7642b620e
annajliang <annaliangdev@gmail.com>
2022-09-19 02:45:15 -0400

    change favicon and add title element

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Normalize } from '../styles/Normalize';
 import { Global } from '../styles/Global';
 import { Animations } from '../styles/Animations';
 import { Layout } from '../components/Layout/Layout';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -12,6 +13,10 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Normalize />
       <Global />
       <Animations />
+      <Head>
+        <title>Who&apos;s That Pokemon?</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
